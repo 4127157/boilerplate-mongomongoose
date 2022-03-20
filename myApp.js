970870705b50cfdd4e8a06e669087be879ea0af8
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-20 14:13:04 +0530

    Find and update age enabled

--- myApp.js
+++ myApp.js
@@ -84,8 +84,11 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+    Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedData) => {
+        if(err)
+            return console.error(err);
+        done(null, updatedData);
+    });
 };
 
 const removeById = (personId, done) => {
